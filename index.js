0e54394e67b5c97211a4406b2fbfc35af27c2bb0
alirezava4idi <alireza.vahidi1@gmail.com>
2024-06-13 22:04:09 +0330

    added nginx

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express()
 const PORT = process.env.PORT;
 
 
+app.get('/api', (req, res) => {
+    res.status(200).json({
+        message: "Welcome to inventory management api!"
+    })
+})
 
 
 app.listen(PORT, () => {
