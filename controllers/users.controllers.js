28306c969f4e67b57417c90d1ae2cc921ada41df
alirezava4idi <alireza.vahidi1@gmail.com>
2024-06-18 12:39:47 +0330

    password hashing added

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -2,6 +2,7 @@ const logger = require('../utils/logger.utils');
 
 const db_pool = require('../database/connection');
 
+const bcrypt = require('bcrypt');
 
 async function signup(req, res)
 {
@@ -9,13 +10,20 @@ async function signup(req, res)
     let password = (req.body.password + "").trim();
     const ipAddr = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     
-    let username_pattern = new RegExp('^[a-zA-Z0-9]{3, 20}$');
-    let password_pattern = new RegExp(`^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,30}$`)
+    let username_pattern = new RegExp('^[a-zA-Z0-9]{3,20}$');
+    let password_pattern = new RegExp(`^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,30}$`);
+
     if (username == "" || password == "" || username == 'undefined' || password == "undefined")    
     {
         logger.error(`${ipAddr} - - ` + "username and/or password cannot empty")
         res.status(400).json({
             error: "username and/or password cannot empty"
+        })
+    }
+    else if (username_pattern.test(username) == false || password_pattern.test(password) == false)
+    {
+        res.status(400).json({
+            error: "username and/or password are the wrong format"
         })
     }
     else
@@ -27,8 +35,9 @@ async function signup(req, res)
             const query = "INSERT INTO `users` (`role`,`username`, `password`, `date_joined`, `last_login`) VALUES (?, ?, ?, ?, ?)"
             const [rows, _fields] = await connection.query(
                 query,
-                [0, username, password, new Date(), new Date()]);
+                [0, username, hashPassword(password), new Date(), new Date()]);
             console.log(rows)
+            console.log(hashPassword(password))
             connection.release();   
             res.status(201).json({
                 message: "User created!"
@@ -51,6 +60,19 @@ async function signup(req, res)
             }
         }
     }
+}
+
+
+
+function hashPassword(password)
+{
+    const hashed = bcrypt.hashSync(password, 10);
+    return hashed;
+}
+
+function verifyPassword(password, hashed)
+{
+    return bcrypt.compareSync(password, hashed)
 }
 
 
