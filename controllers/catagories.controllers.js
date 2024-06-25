68831c0dee994f4d5290362c4d186be8f49c11e6
alirezava4idi <alireza.vahidi1@gmail.com>
2024-06-25 17:34:56 +0330

    create catagory route

--- controllers/catagories.controllers.js
+++ controllers/catagories.controllers.js
@@ -12,4 +12,35 @@ async function get_all_catagories(req, res)
     })
 }
 
-module.exports = { get_all_catagories,}
+async function create_new_catagory(req, res)
+{
+    try {
+     
+        const name = (req.body.name + "").trim();
+        const desc = (req.body.description + "").trim();
+
+        if (name == "" || name == 'undefined')
+        {
+            res.status(400).json({
+                error: ["name is required"]
+            })
+        }else
+        {
+            const connection = await db_pool.getConnection();
+            const query = "INSERT INTO catagory (name, description, created_at, updated_at) VALUES (?,?,?,?);";
+            const [rows, _fields] = await connection.query(query, [name, desc, new Date(), new Date()]);
+            console.log(rows);
+            connection.release();
+            res.status(201).json({
+                message: "Category created successfully",
+                Catagory: {name, desc}
+            })
+        }   
+    } catch (error) {
+        res.status(500).json({
+            error: ["internal server error"]
+        })
+    }
+}
+
+module.exports = { get_all_catagories, create_new_catagory}
--- routes/catagories.routes.js
+++ routes/catagories.routes.js
@@ -5,6 +5,7 @@ const catagories_controllers = require('../controllers/catagories.controllers');
 const protect = require('../middlewares/auth.middleware');
 
 
-router.route('/').get(protect, catagories_controllers.get_all_catagories)
+router.route('/').get(protect, catagories_controllers.get_all_catagories);
+router.route('/').post(protect, catagories_controllers.create_new_catagory);
 
 module.exports = router;
