c248efa3bd340717da704608bf0ba8f2f04d5c8a
alirezava4idi <alireza.vahidi1@gmail.com>
2024-06-26 16:28:02 +0330

    update a catagory

--- controllers/catagories.controllers.js
+++ controllers/catagories.controllers.js
@@ -61,10 +61,7 @@ async function get_catagory_by_id(req, res)
             })
         }else
         {
-            const connection = await db_pool.getConnection();
-            const query = "SELECT * FROM catagory WHERE id = ? LIMIT 1";
-            const [rows, _fields] = await connection.query(query, [catagory_id]);
-            connection.release();
+            const rows = await find_catagory_by_id(catagory_id);
             if (rows.length == 0)
             {
                 res.status(404).json({
@@ -95,4 +92,65 @@ function change_time_zone(date, tz)
 
 }
 
-module.exports = { get_all_catagories, create_new_catagory, get_catagory_by_id}
+async function find_catagory_by_id(catagory_id)
+{
+    const connection = await db_pool.getConnection();
+    const query = "SELECT * FROM catagory WHERE id = ? LIMIT 1";
+    const [rows, _fields] = await connection.query(query, [catagory_id]);
+    connection.release();
+    return rows;
+}
+
+async function update_catagory(req, res)
+{
+    try {
+        const catagory_id = +(req.params.catagoryId + "").trim();
+        if (catagory_id == null)
+        {
+            res.status(404).json({
+                error: ["Category not found"]
+            })
+        }
+        else
+        {
+            const result = await find_catagory_by_id(catagory_id);
+            if (result.length == 0)
+            {
+                res.status(404).json({
+                    error: ["Category not found"]
+                })
+            }
+            else
+            {
+
+                const name = (req.body.name + "").trim();
+                const desc = (req.body.description + "").trim();
+    
+                if (name == "" || name == 'undefined')
+                {
+                    res.status(400).json({
+                        error: ["name is required"]
+                    })
+                }else
+                {
+                    const connection = await db_pool.getConnection();
+                    const query = "UPDATE catagory SET name = ?, description = ?, updated_at = ? WHERE id = ? LIMIT 1;";
+                    const [rows, _fields] = await connection.query(query, [name, desc, new Date(), catagory_id]);
+                    console.log(rows);
+                    connection.release();
+                    res.status(200).json({
+                        message: "Category updated successfully",
+                        Catagory: {name, desc}
+                    })
+                } 
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            error: ["internal server error"]
+        })
+    }
+}
+
+module.exports = { get_all_catagories, create_new_catagory, get_catagory_by_id, update_catagory}
--- routes/catagories.routes.js
+++ routes/catagories.routes.js
@@ -7,6 +7,7 @@ const protect = require('../middlewares/auth.middleware');
 
 router.route('/').get(protect, catagories_controllers.get_all_catagories);
 router.route('/').post(protect, catagories_controllers.create_new_catagory);
-router.route('/:catagoryId').get(protect, catagories_controllers.get_catagory_by_id)
+router.route('/:catagoryId').get(protect, catagories_controllers.get_catagory_by_id);
+router.route('/:catagoryId').put(protect, catagories_controllers.update_catagory);
 
 module.exports = router;
