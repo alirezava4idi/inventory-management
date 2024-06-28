5d4d5f7dea2e52d09bd45c737324ec4dc08dc72c
alirezava4idi <alireza.vahidi1@gmail.com>
2024-06-28 19:01:23 +0330

    Update the details of an existing product

--- controllers/products.controllers.js
+++ controllers/products.controllers.js
@@ -3,7 +3,6 @@ const {change_time_zone} = require('../utils/utils');
 const logger = require('../utils/logger.utils');
 
 const { find_catagory_by_id } = require('./catagories.controllers');
-const e = require('express');
 
 async function get_all_products(req, res)
 {
@@ -127,8 +126,11 @@ async function get_product_by_id(req, res)
             }else
             {
                 logger.info("Retrieve details of a specific product by its ID");
+                product[0].created_at = change_time_zone(product[0].created_at, "Asia/Tehran");
+                product[0].updated_at = change_time_zone(product[0].updated_at, "Asia/Tehran");
                 res.status(200).json({
-                    ...product[0]
+                    ...product[0],
+                    
                 })
             }
         }
@@ -140,4 +142,79 @@ async function get_product_by_id(req, res)
     }
 }
 
-module.exports = {get_all_products, create_product, get_product_by_id,}
+async function update_product_by_id(req, res)
+{
+    try {
+        let productId = (req.params.productId).trim();
+        productId = Number(productId) ? Number(productId) : -1;
+        if (productId == null || productId == 'null' || productId == -1)
+        {
+            logger.error("invalid id in url");
+            res.status(400).json({
+                error: ['bad product id']
+            })
+        }else
+        {
+            
+            const product = await find_product_by_id(productId);
+            if (product.length == 0)
+            {
+                logger.info("Product not found");
+                res.status(404).json({
+                    message: "Product not found"
+                })
+            }
+            else
+            {
+                const name = (req.body.name + '').trim();
+                const description = (req.body.description + '').trim();
+                const price = Number(req.body.price).toFixed(2);
+                const catagoryId = Number(req.body.catagoryId) ? Number(req.body.catagoryId) : -1;
+
+                if (name == 'undefined' || name == undefined || name == '' 
+                    || price == 'NaN' || catagoryId == 'NaN' || catagoryId == NaN 
+                    || price == '' || catagoryId == '' || catagoryId == -1)
+                {
+                    logger.error('invalid name or price or catagoryId')
+                    res.status(400).json({
+                        error: ['invalid request params']
+                    })
+                }
+                else
+                {
+                    const catagory = await find_catagory_by_id(catagoryId)
+                    if (catagory.length == 0)
+                    {
+                        logger.error('catagory does not exist')
+                        res.status(404).json({
+                            error: ['catagory does not exist']
+                        })
+                    }
+                    else
+                    {
+                        const connection = await db_pool.getConnection();
+                        const query = "UPDATE product SET `name` = ?, `description` = ?, `price` = ?, `catagoryId` = ?, `updated_at` = ?";
+                        const [rows, _fields] = await connection.query(query, [name, description, price, catagoryId, new Date()]);
+                        connection.release();
+                        logger.info("product updated")
+                        res.status(201).json({
+                            product: {
+                                name,
+                                description,
+                                price,
+                                catagoryId,
+                            }
+                        })
+                    }
+                }
+            }
+        }
+    } catch (error) {
+        logger.error(error.message);
+        res.status(500).json({
+            error: ["internal server error"]
+        })
+    }
+}
+
+module.exports = {get_all_products, create_product, get_product_by_id, update_product_by_id}
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -8,5 +8,6 @@ const protect = require('../middlewares/auth.middleware');
 router.route('/').get(protect, products_controllers.get_all_products);
 router.route('/').post(protect, products_controllers.create_product);
 router.route('/:productId').get(protect, products_controllers.get_product_by_id);
+router.route('/:productId').put(protect, products_controllers.update_product_by_id);
 
 module.exports = router
